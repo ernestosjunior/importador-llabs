1e06b98f756cc47290a75d5fb4a3291e44fcad46
Ernesto Junior <ernesto.sjunior@hotmail.com>
2025-10-04 01:14:45 -0300

    chore: config vitest coverage

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "~encore": path.resolve(__dirname, "./encore.gen"),
+    },
+  },
+  test: {
+    coverage: {
+      provider: "istanbul",
+    },
+  },
+});
